2d4dc8a9d14f31e69abcb1f65a27355f9fe51172
Dre <dremon7@hotmail.com>
2019-03-08 01:40:13 -0500

    api service and impl

--- src/main/java/guru/springframework/api/domain/User.java
+++ src/main/java/guru/springframework/api/domain/User.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class User implements Serializable
+public class  User implements Serializable
 {
 
     private String gender;
---
+++ src/test/java/guru/springframework/springrestclientexamples/services/ApiServiceImplTest.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrestclientexamples.services;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ApiServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+}
